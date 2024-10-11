1504ac8fef2b0ae4a110dd0ad80f725e8985ddc4
Stephen Yang <stepheny755@gmail.com>
2024-10-11 16:09:25 -0400

    [py] save points to output file. ignore out files

--- convert_svg.py
+++ convert_svg.py
@@ -1,34 +1,69 @@
-import bpy
-import re
+from svg.path import parse_path
+from xml.dom import minidom
+
+import numpy as np
+
+import argparse
+import sys
+import os
 
 
-def svg_circles_to_vertices(filename, scale):
-    verts = []
-    add_vert = verts.append
-
-    pattern = 'cx="(.*)" cy="(.*)" '
-
-    with open(filename) as f:
-        for l in f:
-            stripped = l.strip()
-            if stripped.startswith("<circle"):
-                m = re.search(pattern, stripped)
-                if m:
-                    x, y = m.group(1, 2)
-                    x, y = float(x) * scale, -float(y) * scale
-                    add_vert([x, y, 0])
-
-    # print(verts)
-
-    mesh = bpy.data.meshes.new("mesh_name")
-    mesh.from_pydata(verts, [], [])
-    mesh.update()
-
-    obj = bpy.data.objects.new("obj_name", mesh)
-
-    scene = bpy.context.scene
-    scene.objects.link(obj)
+parser = argparse.ArgumentParser(description="Converts SVG file into 2D (x,y) Points")
+parser.add_argument("-f", "--file", help="SVG file to add", required=True)
+parser.add_argument("-o", "--output", help="Output file name", default="points.csv")
+parser.add_argument("--density", help="Density of SVG points", default=1)
+parser.add_argument("--scale", help="Scale of the image", default=5)
+parser.add_argument("--x_offset", help="x offset for SVG points", default=0)
+parser.add_argument("--y_offset", help="y offset for SVG points", default=0)
+args = parser.parse_args()
 
 
-filename = "10978611521529652583.svg"
-svg_circles_to_vertices(filename, 0.01)
+def get_point_at(path, distance, scale, offset):
+    pos = path.point(distance)
+    pos += offset
+    pos *= scale
+    return pos.real, pos.imag
+
+
+def points_from_path(path, density, scale, offset):
+    step = int(path.length() * density)
+    last_step = step - 1
+
+    if last_step == 0:
+        yield get_point_at(path, 0, scale, offset)
+        return
+
+    for distance in range(step):
+        yield get_point_at(path, distance / last_step, scale, offset)
+
+
+def points_from_doc(doc, density=5, scale=1, offset=0):
+    offset = offset[0] + offset[1] * 1j
+    points = []
+    for element in doc.getElementsByTagName("path"):
+        for path in parse_path(element.getAttribute("d")):
+            points.extend(points_from_path(path, density, scale, offset))
+
+    return points
+
+
+# open svg file
+if not os.path.exists(args.file):
+    print(f"Error: could not find file {args.file}")
+    sys.exit()
+with open(args.file, "r") as file:
+    data = file.read().replace("\n", "")
+
+# parse svg path into xy points
+doc = minidom.parseString(data)
+points = points_from_doc(
+    doc,
+    density=args.density,
+    scale=args.scale,
+    offset=(args.x_offset, args.y_offset),
+)
+doc.unlink()
+
+# save to csv
+points = np.array(points)
+np.savetxt(args.output, points, delimiter=",", fmt="%s")
